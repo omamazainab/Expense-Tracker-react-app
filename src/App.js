c0f5961992bbe622c81ea810b8951afa01c8615d
Omama Zainab <omi.zainy@gmail.com>
2020-06-17 16:09:22 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import {Header} from './components/Header.js';
+import {Balance} from './components/Balance.js';
+import {IncomeExpenses} from './components/IncomeExpenses.js';
+import {TransactionList} from './components/TransactionList.js';
+import {AddTransaction} from './components/AddTransaction.js';
+
+import {GlobalProvider} from './context/GlobalState'
+
+function App() {
+  return (
+    <GlobalProvider >
+      <Header/>
+      <div className="container">
+      <Balance/>
+      <IncomeExpenses />
+      <TransactionList />
+      <AddTransaction />
+      </div>
+      
+    </GlobalProvider>
+  );
+}
+
+export default App;
